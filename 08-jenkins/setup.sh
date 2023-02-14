4593cc96254c896bdf705418d28dcb94eb3d38f0
Alexander Mikhalchenko <alexandr941989@gmail.com>
2023-02-14 15:26:14 +0200

    Add setup script to install Docker and run the Docker compose command.

---
+++ 08-jenkins/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! docker --version &> /dev/null
+then
+    echo "Installing docker..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    rm get-docker.sh
+fi
+
+docker --version
+
+echo "Running Nexus and Jenkins using Docker compose stack..."
+docker compose up -d
+
