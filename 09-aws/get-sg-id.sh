fea4ba1d84d381efb31050ad47bc41b4fec04149
Alexander Mikhalchenko <alexandr941989@gmail.com>
2023-05-13 19:11:00 +0300

    Fix query syntax, it has to be in quotes because of array brackets.

--- 09-aws/get-sg-id.sh
+++ 09-aws/get-sg-id.sh
@@ -1,1 +1,1 @@
-aws ec2 describe-security-groups --filters "Name=tag:Name,Values=node-app-sg" --query SecurityGroups[0].GroupId --output text
+aws ec2 describe-security-groups --filters "Name=tag:Name,Values=node-app-sg" --query 'SecurityGroups[0].GroupId' --output text
