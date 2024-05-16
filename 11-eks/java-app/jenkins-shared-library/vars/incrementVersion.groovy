6e298a0df1b12bfaacfd2ac3ce9ee083a45834cd
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-05-16 11:19:20 +0300

    Update version extraction script due to an error in the previous one.

--- 11-eks/java-app/jenkins-shared-library/vars/incrementVersion.groovy
+++ 11-eks/java-app/jenkins-shared-library/vars/incrementVersion.groovy
@@ -1,7 +1,7 @@
 def call() {
     sh './gradlew incrementVersion --versionIncrementType=MINOR > /dev/null'
     def APP_VERSION = sh (
-        script: "./gradlew printVersion -Psnapshot=false | awk '\$1 ~ /Version: */' | awk 'NF{ print $NF }'",
+        script: "./gradlew printVersion -Psnapshot=false |  awk '/^Version:/ { gsub(/^Version: /, \"\", \$0); print \$0 }'",
         returnStdout: true
     ).trim()
     env.IMAGE_VERSION = "$APP_VERSION-$BUILD_NUMBER"
