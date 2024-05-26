6a672f21a154972472407bcb36425f0c78967f8a
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-05-27 00:34:19 +0300

    Specify kubeconfig file path to fix Helm authentication error.

--- 11-eks/java-app/jenkins-shared-library/vars/deploy.groovy
+++ 11-eks/java-app/jenkins-shared-library/vars/deploy.groovy
@@ -2,16 +2,18 @@ def call(String clusterName, String region) {
     withCredentials([usernamePassword(credentialsId: 'aws', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY')]) {
         sh "aws eks update-kubeconfig --name $clusterName --region $region"
 
+        String kubeconfigPath = '/root/.kube/config'
+
         dir('11-eks/mysql') {
-            sh 'helm upgrade mysql oci://registry-1.docker.io/bitnamicharts/mysql -f mysql-values.yaml'
+            sh "helm upgrade --kubeconfig $kubeconfigPath mysql oci://registry-1.docker.io/bitnamicharts/mysql -f mysql-values.yaml"
         }
 
         dir('11-eks/phpmyadmin') {
-            sh 'helm upgrade phpmyadmin oci://registry-1.docker.io/bitnamicharts/phpmyadmin'
+            sh "helm upgrade --kubeconfig $kubeconfigPath phpmyadmin oci://registry-1.docker.io/bitnamicharts/phpmyadmin"
         }
 
         dir('11-eks/java-app') {
-            sh 'helm upgrade -n java-app java-app java-app-helm-chart'
+            sh "helm upgrade --kubeconfig $kubeconfigPath -n java-app java-app java-app-helm-chart"
         }
     }
 }
