ad5d41fa0f6c09bbe4a5dbe2415126c45240cf83
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-05-27 01:40:23 +0300

    Build chart dependencies for Java app.

--- 11-eks/java-app/jenkins-shared-library/vars/deploy.groovy
+++ 11-eks/java-app/jenkins-shared-library/vars/deploy.groovy
@@ -2,20 +2,20 @@ def call(String clusterName, String region) {
     withCredentials([usernamePassword(credentialsId: 'aws', usernameVariable: 'AWS_ACCESS_KEY_ID', passwordVariable: 'AWS_SECRET_ACCESS_KEY')]) {
         sh "aws eks update-kubeconfig --name $clusterName --region $region"
 
-        String kubeconfigPath = '/root/.kube/config'
-
-        sh "kubectl get nodes"
-
         dir('11-eks/mysql') {
-            sh "helm upgrade mysql oci://registry-1.docker.io/bitnamicharts/mysql -f mysql-values.yaml --kubeconfig $kubeconfigPath --debug"
+            sh "helm upgrade mysql oci://registry-1.docker.io/bitnamicharts/mysql -f mysql-values.yaml"
         }
 
         dir('11-eks/phpmyadmin') {
-            sh "helm upgrade phpmyadmin oci://registry-1.docker.io/bitnamicharts/phpmyadmin --kubeconfig $kubeconfigPath --debug"
+            sh "helm upgrade phpmyadmin oci://registry-1.docker.io/bitnamicharts/phpmyadmin"
+        }
+
+        dir('11-eks/java-app/java-app-helm-chart') {
+            sh "helm dependency build"
         }
 
         dir('11-eks/java-app') {
-            sh "helm upgrade -n java-app java-app java-app-helm-chart --kubeconfig $kubeconfigPath --debug"
+            sh "helm upgrade -n java-app java-app java-app-helm-chart"
         }
     }
 }
