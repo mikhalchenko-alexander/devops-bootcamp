546b2429d1c4ba07071ea8b10818124ac750c8af
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-11-25 12:36:16 +0200

    Add vpc and cluster basic config.

--- 12-terraform/terraform/main.tf
+++ 12-terraform/terraform/main.tf
@@ -2,8 +2,77 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+locals {
+  app_name_prefix = "java-app-eks"
+  cluster_name = "${local.app_name_prefix}-${terraform.workspace}"
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.2.0"
+
+  name                 = "${local.app_name_prefix}-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.29.0"
-}
 
+  cluster_version = "1.29"
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {}
+  }
+
+  eks_managed_node_groups = {
+    ng1 = {
+      iam_role_additional_policies = {
+        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+
+      instance_types = ["m5.large"]
+      min_size = 1
+      max_size = 3
+    }
+  }
+
+  fargate_profiles = {
+    default = {
+      name = "fp-default"
+
+      selectors = [
+        {
+          namespace = "java-app"
+        }
+      ]
+    }
+  }
+
+  cluster_name = local.cluster_name
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+}
