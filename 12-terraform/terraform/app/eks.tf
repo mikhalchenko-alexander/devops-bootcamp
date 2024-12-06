f438e248e2a6e20ff63666976df6336be4265265
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-12-06 11:49:19 +0200

    Refactor to modules, add output.

---
+++ 12-terraform/terraform/app/eks.tf
@@ -0,0 +1,40 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.29.0"
+
+  cluster_version = "1.29"
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {}
+  }
+
+  eks_managed_node_groups = {
+    ng1 = {
+      iam_role_additional_policies = {
+        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+
+      node_group_name = "ng-1"
+      instance_types = ["m5.large"]
+      min_size        = 1
+      max_size        = 3
+      desired_size    = 3
+    }
+  }
+
+  fargate_profiles = {
+    default = {
+      name = "fp-default"
+
+      selectors = [
+        {
+          namespace = "java-app"
+        }
+      ]
+    }
+  }
+
+  cluster_name = local.cluster_name
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.private_subnets
+}
