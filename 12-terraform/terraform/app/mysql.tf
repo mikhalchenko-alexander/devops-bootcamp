0fc132182d1c854f5ed0b8e70046c051757b4196
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-12-06 12:00:25 +0200

    Add MySQL chart.

---
+++ 12-terraform/terraform/app/mysql.tf
@@ -0,0 +1,39 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks.cluster_name]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks.cluster_name]
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+provider "helm" {
+  kubernetes {
+    host = data.aws_eks_cluster.cluster.endpoint
+    token = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
+}
+
+resource "helm_release" "mysql" {
+  name       = "mysql"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "bitnami/mysql"
+  version    = "12.1.0"
+
+  values = [
+    file("mysql-values.yaml")
+  ]
+
+  set {
+    name  = "volumePermissions.enabled"
+    value = true
+  }
+}
