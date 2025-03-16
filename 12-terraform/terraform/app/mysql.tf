6dca0e7f72985f530d13c67d59c7784c8dd41355
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-03-16 16:17:32 +0200

    Remove array access to follow docs.

--- 12-terraform/terraform/app/mysql.tf
+++ 12-terraform/terraform/app/mysql.tf
@@ -11,12 +11,12 @@ data "aws_eks_cluster_auth" "cluster" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.data)
 }
 
 provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.data)
   load_config_file       = false
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
@@ -25,7 +25,7 @@ provider "helm" {
   kubernetes {
     host = data.aws_eks_cluster.cluster.endpoint
     token = data.aws_eks_cluster_auth.cluster.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.data)
   }
 }
 
