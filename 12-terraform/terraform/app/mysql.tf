be55727b55f0dbfdba8465344740baa74f3407b9
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-03-31 13:03:44 +0300

    Simplify config.

--- 12-terraform/terraform/app/mysql.tf
+++ 12-terraform/terraform/app/mysql.tf
@@ -16,13 +16,9 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    host = data.aws_eks_cluster.cluster.endpoint
+    host  = data.aws_eks_cluster.cluster.endpoint
+    token = data.aws_eks_cluster_auth.cluster.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
   }
 }
 
