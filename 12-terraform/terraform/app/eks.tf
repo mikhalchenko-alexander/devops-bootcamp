e9b8152dc5e9a2820b67a396fc95c149f7e4dc22
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-06-14 10:43:17 +0300

    Add an explicit CIDR block to try to fix internal DNS issues.

--- 12-terraform/terraform/app/eks.tf
+++ 12-terraform/terraform/app/eks.tf
@@ -66,11 +66,12 @@ module "eks" {
 }
 
 resource "aws_security_group_rule" "eks-node-ingress-cluster-dns" {
-  description = "Allow pods DNS"
+  description              = "Allow pods DNS"
   from_port                = 53
   protocol                 = 17
   security_group_id        = module.eks.cluster_security_group_id
   source_security_group_id = module.eks.cluster_security_group_id
   to_port                  = 53
   type                     = "ingress"
+  cidr_blocks = [module.vpc.vpc_cidr_block]
 }
