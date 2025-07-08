f94fd8fca2f9b2716226e31e5266d810f1588805
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-07-08 10:17:49 +0300

    Temporarily remove security group rules as they should be added separately. The current setup creates circular dependency as module.eks.cluster_security_group_id does not exist at this point.

--- 12-terraform/terraform/app/eks.tf
+++ 12-terraform/terraform/app/eks.tf
@@ -34,35 +34,6 @@ module "eks" {
     }
   }
 
-  node_security_group_additional_rules = {
-    fargate_nodes_to_dns_tcp = {
-      description = "Fargate nodes to cluster DNS TCP"
-      protocol    = "tcp"
-      from_port   = 53
-      to_port     = 53
-      type        = "ingress"
-      source      = module.eks.cluster_security_group_id
-    }
-
-    fargate_nodes_to_dns_udp = {
-      description = "Fargate nodes to cluster DNS UDP"
-      protocol    = "udp"
-      from_port   = 53
-      to_port     = 53
-      type        = "ingress"
-      source      = module.eks.cluster_security_group_id
-    }
-
-    fargate_nodes_to_dns_tcp = {
-      description = "Fargate nodes to MySQL nodes"
-      protocol    = "tcp"
-      from_port   = 3306
-      to_port     = 3306
-      type        = "ingress"
-      source      = module.eks.cluster_security_group_id
-    }
-  }
-
   access_entries = {
     jenkins = {
       principal_arn = var.jenkins_user_arn
