93ca550f9213cb442c1e298686e3b4ace9e9ece6
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-07-22 13:23:03 +0300

    Reformat code.

--- 13-programming-with-python/exercise_04_module.py
+++ 13-programming-with-python/exercise_04_module.py
@@ -5,6 +5,7 @@ def print_youngest_employee(employees):
             youngest_employee = employee
     print(f"Name: {youngest_employee['name']}, Age: {youngest_employee['age']}")
 
+
 def count_uppercase_lowecase_letters(text):
     uppercase_letters = 0
     lowercase_letters = 0
@@ -14,6 +15,7 @@ def count_uppercase_lowecase_letters(text):
         elif letter.islower():
             lowercase_letters += 1
     return uppercase_letters, lowercase_letters
+
 
 def print_even_numbers(numbers):
     even_numbers = []
