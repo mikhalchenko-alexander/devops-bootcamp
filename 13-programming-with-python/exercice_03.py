e26b32f38408dfff168f5e8fac36ea678c07246c
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-07-22 12:20:11 +0300

    Complete exercise 3.

---
+++ 13-programming-with-python/exercice_03.py
@@ -0,0 +1,25 @@
+employees = [{
+    "name": "Tina",
+    "age": 30,
+    "birthday": "1990-03-10",
+    "job": "DevOps Engineer",
+    "address": {
+        "city": "New York",
+        "country": "USA"
+    }
+},
+    {
+        "name": "Tim",
+        "age": 35,
+        "birthday": "1985-02-21",
+        "job": "Developer",
+        "address": {
+            "city": "Sydney",
+            "country": "Australia"
+        }
+    }]
+
+for employee in employees:
+    print(f"Name: {employee['name']}, Job: {employee['job']}, City: {employee['address']['city']}")
+
+print(f"Second employee country: {employees[1]['address']['country']}")
