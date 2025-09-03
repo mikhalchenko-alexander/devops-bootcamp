c9c17af15f7c6deaab6197c5b28f61cf4c08ed49
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-09-03 15:54:10 +0300

    Complete server provision and configuration.

--- 15-ansible/exercise6/run.sh
+++ 15-ansible/exercise6/run.sh
@@ -2,5 +2,5 @@
 
 ansible-playbook -e "@vars.yaml" provision_ansible_instance.yaml
 ansible-playbook -e "@vars.yaml" provision_app_instances.yaml
-#ansible-playbook -i inventory_aws_ec2.yaml -e "@vars.yaml" configure_ansible_instance.yaml
-#ansible-playbook -i inventory_aws_ec2.yaml -e "@vars.yaml" configure_app_instances.yaml
+ansible-playbook -i inventory_aws_ec2.yaml -e "@vars.yaml" configure_ansible_instance.yaml
+ansible-playbook -i inventory_aws_ec2.yaml -e "@vars.yaml" configure_app_instances.yaml
